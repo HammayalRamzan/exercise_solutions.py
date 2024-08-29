149968fb037369b66353143fd09e95d878bfb371
Hammayal <hammayalramzan@gmail.com>
2024-08-29 11:55:20 +0500

    Add files via upload

---
+++ exercise_solutions.py
@@ -0,0 +1,116 @@
+# Assign a message to a variable 
+message ="python_learning"
+# Print the message
+print(message)
+# Assign a message to a variable
+message_2 ="processing"
+# Print the message
+print(message_2)
+# Change the value of variable
+message_3 ="hello_world.py"
+# Print the new message
+print(message_3)
+# Use a variable to represent person name
+name=("hamayal")
+# Print a message
+print(name)
+# Print a message to that person
+print(f"Hello, {name}, would you like to learn some Python today?")
+# Use variable to represent person name
+person_name1 ="rida"
+# Print a message to new person
+print(f"Hello,{person_name1}, How are you")
+# Print the person name in lowercase,upper case and title case
+name= "hamayal ramzan"
+print(name.title())
+print(name.upper())
+print(name.lower())
+# Assign a qoute from a famous person
+qoute="A person who never made a mistake never tried anything new"
+author="albert einstein"
+# Print the qoute and name of its author
+print(f"{qoute}{author}")
+# Assign the famous person name
+famous_person="Abraham Lincoln"
+# Assign the message of famous person
+message_4="The best way to predict your future is to create it"
+# Print the message
+f"{famous_person}{message_4}"
+# Assign a name with whitespace character
+person_name="   hamayal    "
+person_name1="  \n\thamayal   "
+# Pint the name once, so the whitespace around the name is displayed
+print(person_name)
+print(person_name1)
+# Print the name using each of the three stripping functions
+print(person_name.lstrip())
+print(person_name.rstrip())
+print(person_name.strip())
+# Assign the values
+x=5
+y=10
+z=15
+sum=x+y+z
+# Print their sum
+print(sum)
+# Swap the values of two variables a and b
+a=5
+b=10
+# Print the values before and after the swap
+print(f"before swap,a={a} and b={b}")
+print(f"after swap,a={a} and b={b}")
+# Assign a variable with your favorite color 
+favorite_color="Black"
+# Print it
+print(favorite_color)
+# change the variable name to something else
+color="favorite_color"
+# Print the color again
+print(color)
+# Assign a variable pet_name and set it to "buddy"
+pet_name="Buddy"
+# change the value of pet name
+pet_name2="Max"
+# Print the new value 
+print(pet_name2)
+# Assign the value "Sunshine" to a variable
+weather=("Sunshine")
+# Print the value of variable
+print(weather)
+# Mistakenly try to print a different variable
+print("sunshine")
+# Assign the value 100 to a variable score
+score_1="100"
+# Print it
+print(score_1)
+# Assign a new value to score
+score_2="150"
+# Print it again
+print(score_2)
+# Assign it the name of a city you like
+city="Islamabad"
+# Print the city name
+print(city)
+# Assign a variable text with the value
+text="Python learning"
+# Print it in title case
+print(text.title())
+# Assign a string with mixed cases to a variable 
+mixed_cases1="Hello world"
+# print it in lowercase letter
+print(mixed_cases1.lower())
+# Assign a string with mixed cases to a variable
+mixed_cases2="Hello WorLd"
+# Print it in uppercase letter
+print(mixed_cases2.upper())
+# Assign a variable temperature 
+temperature=25
+# print it
+print(f"the current temperature is{temperature}degrees")
+# Assign a short poem that has multiple lines
+poem= """In the quiet of the night,
+Stars twinkle in the sky's embrace,
+Whispers of dreams take gentle flight,
+Moonlight dances on the lake's face."""
+# Print the poem
+print(poem)
